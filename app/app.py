1236f05e1265828df80fb3c0e27d156e3d84d6ec
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-12-31 21:25:21 +0530

    app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+from flask_uploads import *
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['img']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return redirect(UPLOAD_FOLDER + "/" + filename)
 
 
 
